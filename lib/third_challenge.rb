0372920d8c45eacad2e12e5957594918ca02dcfc
Zachary Schulz <zjschulz@gmail.com>
2019-08-30 22:48:50 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,10 +2,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montague", age: "54"
         },
       :hero => {
         #your key/value pairs here
