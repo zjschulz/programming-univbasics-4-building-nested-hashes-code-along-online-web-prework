c05c5a6fef70b607cdbc5c3418e9006ec489766e
Zachary Schulz <zjschulz@gmail.com>
2019-08-30 22:55:04 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,7 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {}{}
+        {},
+        {}
       ]
    }
 }
