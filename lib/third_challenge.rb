676887a34b7a7a5c061bb7fea4b90f57ccde5e2e
Zachary Schulz <zjschulz@gmail.com>
2019-08-30 22:49:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
         name: "Lady Montague", age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
